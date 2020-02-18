b3197098e96ed0600053b60d4ac7fbc442434c5d
n01e0 <noleo@vanillabeans.mobi>
2020-02-19 00:34:40 +0900

    hostname whoami path branch

--- src/rush.rs
+++ src/rush.rs
@@ -1,5 +1,6 @@
 extern crate colored;
 extern crate dirs;
+extern crate hostname;
 
 use colored::*;
 use std::env;
@@ -8,6 +9,7 @@ use std::io::{Write, stdout};
 use std::process::{ Command, exit };
 
 pub struct Shell {
+    user: String,
     cwd: PathBuf,
     branch: String,
     code: i32,
@@ -17,6 +19,7 @@ pub struct Shell {
 impl Shell {
     pub fn new() -> Shell {
         Shell {
+            user: String::new(),
             cwd: PathBuf::new(),
             branch: String::new(),
             code: 0,
@@ -25,20 +28,25 @@ impl Shell {
     }
 
     pub fn flush(&mut self) -> &mut Shell {
+        self.user = if let Ok(output) = 
+            Command::new("whoami").output() {
+                let mut name = output.stdout;
+                name.retain(|c| *c != 0xa);
+                format!("{}/{}>", hostname::get().unwrap().into_string().unwrap(), String::from_utf8(name).unwrap())
+        }else{"".to_string()};
         self.cwd = env::current_dir().unwrap();
         self.branch = if let Ok(output) = 
             Command::new("git").args(&["symbolic-ref", "--short", "HEAD"]).output() {
                 let mut currnt_branch = output.stdout;
                 currnt_branch.retain(|c| *c != 0xa);
                 format!("[{}]", 
-                        String::from_utf8(currnt_branch)
-                        .unwrap_or("".to_string()))
-            } else {
-                "".to_string()
-            };
+                        String::from_utf8(currnt_branch).unwrap_or("".to_string())
+                )
+        }else{"".to_string()};
+        let user = format!("{}", self.user).on_green();
         let cwd = format!("{}", self.cwd.to_str().unwrap()).red().on_cyan();
         let branch = format!("{}", self.branch).on_red();
-        println!("{}{}", cwd, branch);
+        println!("{}{}{}", user, cwd, branch);
         print!("{}", self.prompt);
         stdout().flush().unwrap();
         self
@@ -148,27 +156,27 @@ fn chdir(args: Option<Vec<&str>>) -> i32 {
     }
 }
 
-fn alias(args: Option<Vec<&str>>) -> i32 {
-    match args {
-        Some(command) => {
-            match command.len() {
-                1 => {
-                    let command: Vec<&str> = command[0].split('=').collect();
-                    if command.len() == 2 {
-                        env::set_var(command[0], command[1]);
-                    } else {
-                        eprintln!("{}", format!("Usage: setenv <KEY=VALUE|KEY VALUE>").red());
-                    }
-                },
-                2 => env::set_var(command[0], command[1]),
-                _ => eprintln!("{}", format!("Usage: setenv <KEY=VALUE|KEY VALUE>").red()),
-            }
-        },
-        None => eprintln!("{}", format!("Usage: setenv <KEY=VALUE|KEY VALUE>").red()), 
-    }
-
-    return 0;
-}
+//fn alias(args: Option<Vec<&str>>) -> i32 {
+//    match args {
+//        Some(command) => {
+//            match command.len() {
+//                1 => {
+//                    let command: Vec<&str> = command[0].split('=').collect();
+//                    if command.len() == 2 {
+//                        env::set_var(command[0], command[1]);
+//                    } else {
+//                        eprintln!("{}", format!("Usage: setenv <KEY=VALUE|KEY VALUE>").red());
+//                    }
+//                },
+//                2 => env::set_var(command[0], command[1]),
+//                _ => eprintln!("{}", format!("Usage: setenv <KEY=VALUE|KEY VALUE>").red()),
+//            }
+//        },
+//        None => eprintln!("{}", format!("Usage: setenv <KEY=VALUE|KEY VALUE>").red()), 
+//    }
+//
+//    return 0;
+//}
 
 fn launch(command: &str, args: Option<Vec<&str>>) -> i32 {
     let mut proc = Command::new(command);
